66f854147c05146f7461363343bcbba2bd04b9e6
Padmaja Gogulamudi <21bq1a1246@vvit.net>
2025-08-13 11:57:06 +0530

    13-8-2025 practice

--- CASE_STUDY.sql
+++ CASE_STUDY.sql
@@ -6,4 +6,10 @@ TIMESTAMPDIFF(YEAR, E.HIREDATE, CURDATE())
 FROM EMP E JOIN EMP M ON E.MGR=M.EMPNO JOIN DEPT D ON E.DEPTNO=D.DEPTNO
 WHERE E.MGR IS NOT NULL
 ORDER BY D.DNAME,E.ENAME;
+select * from emp;
+# 10-8-2025 creating Procedure named adjust_salary_info
 
+update emp set sal=1560  where empno=7782;
+set @p_increment=2;
+call adjust_salary_info(7782,@p_increment,@p_new_salary);
+select @p_new_salary as updates_sal;
