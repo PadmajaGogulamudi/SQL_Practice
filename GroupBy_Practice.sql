54cf5d8f1fef2ed7f0c7710a6b0258a9547a6a2e
Padmaja Gogulamudi <21bq1a1246@vvit.net>
2025-08-19 14:36:52 +0530

    19-8-2025 adding practice

--- GroupBy_Practice.sql
+++ GroupBy_Practice.sql
@@ -113,3 +113,63 @@ select d.loc,count(e.ename) as no_of_employees from emp e right join dept d on e
 select e.deptno,e.job,count(*) from emp e join dept d on e.deptno=d.deptno group by e.deptno,e.job order by e.deptno;
 #24. Show total salary paid in each department by location.
 select d.deptno,d.loc,sum(e.sal) from emp e join dept d on e.deptno=d.deptno group by e.deptno;
+
+#List department names where average salary is greater than 2000.
+select d.dname from emp e join dept d  group by e.deptno,d.dname having avg(e.sal)>2000;
+
+#List total salary for each job in each department.
+select deptno,job,sum(sal),count(ename) from emp group by deptno,job order by deptno;
+#Count employees by job and manager.
+select job,mgr,count(*) from emp group by job,mgr;
+#28. Show department and job-wise average salary.
+select deptno,round(avg(sal),2) as avarage_sal from emp group by deptno,job;
+
+#29. List department and job combinations where max salary exceeds 3000.
+select deptno,job,max(sal) from emp group by deptno,job having max(sal)>3000;
+#30. Count employees grouped by DEPTNO and JOB.
+select deptno,job,count(*) as no_of_emps from emp group by deptno,job;
+# List departments having more than 1 employee and total salary above 5000.
+SELECT  
+    deptno
+FROM emp
+GROUP BY deptno
+HAVING COUNT(ename) > 1 
+   AND SUM(sal) > 5000;
+   
+#Find jobs where sum of salary is greater than 6000.
+SELECT 
+	job
+FROM emp
+GROUP BY job
+HAVING  SUM(sal)>6000;
+
+#33. Show department/job groups where count of employees is more than 1 and avg salary < 2500.
+select deptno,job from emp group by deptno,job having count(*)>1 and avg(sal)<2500;
+
+#34. Show job roles with more than 2 employees and max salary below 3000.
+select job from emp group by job having count(*)>2 and max(sal)<3000;
+#35. Show managers who manage more than 2 employees with average salary > 1500.
+select m.ename as manager,count(e.ename ) as no_of_emps from emp e join emp m on e.mgr=m.empno group by m.empno having count(e.empno)>2 and avg(e.sal)>1500;
+-- SELECT 
+--     m.empno,
+--     m.ename AS Manager,
+--     COUNT(e.empno) AS No_of_Employees,
+--     ROUND(AVG(e.sal), 2) AS Avg_Salary
+-- FROM emp e
+-- JOIN emp m ON e.mgr = m.empno
+-- GROUP BY m.empno, m.ename
+-- HAVING COUNT(e.empno) > 2 
+--    AND AVG(e.sal) > 1500;
+#36. Show department-wise average salary but only include employees with salary > 1000.
+select deptno,avg(sal) from emp where sal>1000 group by deptno ;
+
+#37. List job-wise total commission where commission is not null.
+select job,sum(comm) from emp where comm is not null group by job;
+#38. Count employees hired after 01-Jan-1981 grouped by department.
+select deptno,count(ename) from emp where hiredate>STR_TO_DATE('01-01-1981', '%d-%m-%Y') group by deptno;
+#39. Show department-wise total salary excluding SALESMANs.
+select deptno,sum(sal) from emp where job<>"SALESMAN" group by deptno;
+#!= and <> both are same but <> is standard it works in most of the databases. so try to use <>
+#40. Show average salary per department for employees earning more than 2000.
+select deptno,avg(sal) from emp where sal>2000 group by deptno;
+
--- file5.sql
+++ file5.sql
@@ -3,3 +3,5 @@ call Check_Even_or_Odd(4);
 call Sal_update_on_bonus();
 
 call Count_emp_sal_gre_1500_ineachDept();
+
+call Nth_Highest_sal(1);
